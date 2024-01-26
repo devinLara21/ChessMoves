ae7b96fd12bc70c842d44285679223beecfedc1b
devinlara21 <dmlara5@miners.utep.edu>
2024-01-26 15:38:48 -0700

    Updated sys

--- Main.java
+++ Main.java
@@ -2,5 +2,6 @@
 class Main{
    public static void main(String[] args){
         System.out.println("Hello World ");
+        System.out.println(" this is to see if it changed the git thingy");
    }
 } 
