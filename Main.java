efeadccbc730f30cee8791c611f78e76fe72feb9
devinlara21 <dmlara5@miners.utep.edu>
2024-01-26 19:51:56 -0700

    is working

--- Main.java
+++ Main.java
@@ -1,15 +1,15 @@
 
 class Main{
    public static void main(String[] args){
-      
+     
    }
    public static boolean isValidSquare(int row, int col){
       return row >= 0 && row < 8 && col >= 0 && col < 8;
    }
 
-   
-   public static boolean isRookMoveValid(String a, int b){
+
+   /*public static boolean isRookMoveValid(String a, int b){
      
    
-      }
-} 
+   */
+  }
